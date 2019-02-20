f0741fd6d78b8e0702e70d343d4c2410fec3a39f
Aoranun <aiaoy1479@gmail.com>
2019-02-20 21:44:35 +0700

    Update Functions in view

--- myapp/models.py
+++ myapp/models.py
@@ -2,24 +2,24 @@ from django.db import models
 from django.utils import timezone
 
 class Review(models.Model):
-    title = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    title = models.TextField(max_length=100)
+    rating = models.IntegerField(default=0)
     writer_name = models.CharField(max_length=50)
-    body = models.CharField(max_length=600)
+    body = models.TextField(max_length=600)
     review_date = models.DateTimeField('date published')
 
 class Cate(models.Model):
-    shop_cate = models.CharField(max_length=50)
-    food_cate = models.CharField(max_length=50)
+    shop_cate = models.TextField(max_length=50)
+    food_cate = models.TextField(max_length=50)
 
 class Menu(models.Model):
-    menu = models.CharField(max_length=50)
-    cost = models.IntegerField()
+    menu = models.TextField(max_length=50)
+    cost = models.IntegerField(default=0)
 
 class Shop(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.TextField(max_length=100)
+    locate = models.TextField(max_length=100)
+    note = models.TextField(max_length=200)
     cate = models.ForeignKey(Cate, on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    locate = models.CharField(max_length=100)
-    note = models.CharField(max_length=200)
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from .models import Review, Cate, Menu, Shop
 
 def index(request):
-    return render(request, 'index.html')
+    last_review = Review.objects.all().last()[0:6]
+    return render(request, 'index.html', {'last_review': last_review})
 
 def cate(request):
     return render(request, 'cate.html')
 
-def review(request):
-    return render(request, 'reviews.html')
+def review(request,shop_name):
+    shops = Shop.objects.get(name=shop_name)
+    last_review = Review.object.filter(name=x).last()[0:3]
+    if request.POST:
+        title = shop_name
+        rating = request.POST['rating']
+        body = request.POST['discuss']
+        wriname = request.POST['name']
+        review = Review(titles=title, ratings=rating, writer_names=wriname, bodys=body)
+        review.save()
+    return render(request, 'reviews.html',{'shop': shops, 'shop_name':shop_name, 'last_review': last_review})
 
 def search(request):
-    return render(request, 'search.html')
+    shops = ""
+    if request.GET:
+        shops = Shop.objects.filter(name=request.GET['q'])
+    return render(request, 'search.html', {'shop': shops})
